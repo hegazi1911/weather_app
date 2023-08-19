067523da5a5f8f4e2a6daf520bc4c768b9c61504
Ahmed Hegazi <ahmedhegazi26@gmail.com>
2023-08-19 16:25:34 +0300

    added dio

--- lib/Services/weather_services.dart
+++ lib/Services/weather_services.dart
@@ -1,20 +1,19 @@
-import 'dart:convert';
+import 'dart:developer';
 
-import 'package:http/http.dart' as http;
+import 'package:dio/dio.dart';
 import 'package:weather_app/Model/weather_model.dart';
 
 class WeatherServices {
+  final Dio dio = Dio();
   Future<WeatherModel?> getWeather({required String cityName}) async {
     String baseUrl = "http://api.weatherapi.com/v1";
     String apikey = "b0683afe1f39498699a181622230605";
-    Uri url =
-        Uri.parse("$baseUrl/forecast.json?key=$apikey&q=$cityName&days=7");
+    String url = "$baseUrl/forecast.json?key=$apikey&q=$cityName&days=7";
     WeatherModel? weather;
     try {
-      http.Response response = await http.get(url);
-      Map<String, dynamic> data = jsonDecode(response.body);
-
-      weather = WeatherModel.fromjson(data);
+      Response response = await dio.get(url);
+      weather = WeatherModel.fromjson(response.data);
+      log(response.data.toString());
     } catch (e) {}
     return weather;
   }
